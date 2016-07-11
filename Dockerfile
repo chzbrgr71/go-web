b8bff9314ec031cf5706a75b00ae2f11dffd2cdc
Brian Redmond <brian.redmond@live.com>
2016-07-11 15:29:30 -0400

    Updated to use indexHandler. New docker file

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,7 @@
-# Start from a Debian image with the latest version of Go installed
-# and a workspace (GOPATH) configured at /go.
 FROM golang
-
-# Copy the local package files to the container's workspace.
-ADD . /go/src/go-web
-
-# Build golang web app 
-RUN go install go-web 
-
-# Run the go-web app by default when the container starts
-ENTRYPOINT go-web
-
-# Document that the service listens on port 8080.
+ 
+ADD ./src /go/src/go-hello-web
+RUN go install go-hello-web
+ENTRYPOINT /go/bin/go-hello-web
+ 
 EXPOSE 8001
---
+++ src/main.go
@@ -0,0 +1,40 @@
+package main
+ 
+import(
+"fmt"
+"os"
+"net/http"
+"net"
+)
+
+func indexHandler( w http.ResponseWriter, r *http.Request){
+    var hostname string
+    var ipaddress string
+    hostname = get_hostname()
+    addrs, err := net.LookupIP(hostname)
+    if err != nil {
+        //add error handling
+    } else {
+        for _, a := range addrs {
+            ipaddress = a.String()
+        }
+    }
+    fmt.Fprintf(w, "<html><h2>Simple Golang Web App</h2><p>This is Brian Redmond's web app</p><p>Version 1.0</p><p>Hostname: %s</p><p>IP address: %s</p><br><img src='http://natebrennand.github.io/concurrency_and_golang/pics/gopher_head.png'></html>", hostname, ipaddress)
+}
+ 
+func main(){
+    http.HandleFunc("/", indexHandler)
+    http.ListenAndServe(":8080",nil)
+}
+
+func get_hostname() string {
+    var result string
+    localhost_name, err := os.Hostname()
+
+    if err != nil {
+        result = "ERROR: Cannot find server hostname"
+    } else {
+        result = localhost_name
+    }
+    return result
+}
